3612f2b5a930c27a0180cc54fb43132c185a4f0c
Idklol55 <95130098+Idklol55@users.noreply.github.com>
2026-04-05 18:36:27 +0700

    Update StorageUtil.hx

--- src/funkin/mobile/backend/StorageUtil.hx
+++ src/funkin/mobile/backend/StorageUtil.hx
@@ -22,6 +22,18 @@
 
 package funkin.mobile.backend;
 
+#if android
+import android.content.Context as AndroidContext;
+import android.widget.Toast as AndroidToast;
+import android.os.Environment as AndroidEnvironment;
+import android.Permissions as AndroidPermissions;
+import android.Settings as AndroidSettings;
+import android.Tools as AndroidTools;
+import android.os.Build.VERSION as AndroidVersion;
+import android.os.Build.VERSION_CODES as AndroidVersionCode;
+import android.os.BatteryManager as AndroidBatteryManager;
+#end
+
 /**
  * A storage class for mobile.
  * @author Karim Akra and Homura Akemi (HomuHomu833)
