f9f3c452a72fd335fb38cd7ad6e5d9a38dc7bcb8
Idklol55 <95130098+Idklol55@users.noreply.github.com>
2026-04-05 07:10:39 +0700

    Update Main.hx

--- src/Main.hx
+++ src/Main.hx
@@ -29,7 +29,7 @@ class Main extends Sprite {
 	public function new() {
 		super();
 
-		Lib.current.loaderInfo.uncaughtErrorEvents.addEventListener(UncaughtErrorEvent.UNCAUGHT_ERROR, onCrash);
+		//Lib.current.loaderInfo.uncaughtErrorEvents.addEventListener(UncaughtErrorEvent.UNCAUGHT_ERROR, onCrash);
 
 		addChild(new FlxGame(InitState, 1280, 720, 60, true));
 		addChild(fpsCounter = new FPSCounter(10, 10, 12));
