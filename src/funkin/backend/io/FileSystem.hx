a8b199da49f618edf00a6025162f84fcb34a3a12
Idklol55 <95130098+Idklol55@users.noreply.github.com>
2026-04-06 16:15:54 +0700

    Update FileSystem.hx

--- src/funkin/backend/io/FileSystem.hx
+++ src/funkin/backend/io/FileSystem.hx
@@ -1,4 +1,4 @@
-package backend.io;
+package funkin.backend.io;
 
 import openfl.Assets;
 #if sys
@@ -35,7 +35,7 @@ class FileSystem
 
 	public static function exists(path:String):Bool
 	{
-		#if FEATURE_MODS
+		#if ADDONS_ALLOWED
 		#if linux
 		var actualPath:String = cwd(path);
 		actualPath = getCaseInsensitivePath(path);
@@ -57,7 +57,7 @@ class FileSystem
 
 	public static function rename(path:String, newPath:String):Void
 	{
-		#if FEATURE_MODS
+		#if ADDONS_ALLOWED
 		#if linux
 		var actualPath:String = cwd(path);
 		actualPath = getCaseInsensitivePath(path);
@@ -74,7 +74,7 @@ class FileSystem
 
 	public static function stat(path:String):Null<#if sys FileStat #else Dynamic #end>
 	{
-		#if FEATURE_MODS
+		#if ADDONS_ALLOWED
 		#if linux
 		var actualPath:String = cwd(path);
 		actualPath = getCaseInsensitivePath(path);
@@ -91,7 +91,7 @@ class FileSystem
 
 	public static function fullPath(path:String):String
 	{
-		#if FEATURE_MODS
+		#if ADDONS_ALLOWED
 		#if linux
 		var actualPath:String = cwd(path);
 		actualPath = getCaseInsensitivePath(path);
@@ -108,7 +108,7 @@ class FileSystem
 
 	public static function absolutePath(path:String):String
 	{
-		#if FEATURE_MODS
+		#if ADDONS_ALLOWED
 		#if linux
 		var actualPath:String = cwd(path);
 		actualPath = getCaseInsensitivePath(path);
@@ -125,7 +125,7 @@ class FileSystem
 
 	public static function isDirectory(path:String):Bool
 	{
-		#if FEATURE_MODS
+		#if ADDONS_ALLOWED
 		#if linux
 		var actualPath:String = cwd(path);
 		actualPath = getCaseInsensitivePath(path);
@@ -144,7 +144,7 @@ class FileSystem
 
 	public static function createDirectory(path:String):Void
 	{
-		#if FEATURE_MODS
+		#if ADDONS_ALLOWED
 		if (!SysFileSystem.exists(cwd(path)))
 			SysFileSystem.createDirectory(cwd(path));
 		#end
@@ -152,7 +152,7 @@ class FileSystem
 
 	public static function deleteFile(path:String):Void
 	{
-		#if FEATURE_MODS
+		#if ADDONS_ALLOWED
 		#if linux
 		var actualPath:String = cwd(path);
 		actualPath = getCaseInsensitivePath(path);
@@ -169,7 +169,7 @@ class FileSystem
 
 	public static function deleteDirectory(path:String):Void
 	{
-		#if FEATURE_MODS
+		#if ADDONS_ALLOWED
 		#if linux
 		var actualPath:String = cwd(path);
 		actualPath = getCaseInsensitivePath(path);
@@ -186,7 +186,7 @@ class FileSystem
 
 	public static function readDirectory(path:String):Array<String>
 	{
-		#if FEATURE_MODS
+		#if ADDONS_ALLOWED
 		#if linux
 		var actualPath:String = cwd(path);
 		actualPath = getCaseInsensitivePath(path);
@@ -226,7 +226,7 @@ class FileSystem
 		return results.map(f -> f.substr(f.lastIndexOf("/") + 1));
 	}
 
-	#if (linux && FEATURE_MODS)
+	#if (linux && ADDONS_ALLOWED)
 	static function getCaseInsensitivePath(path:String):String
 	{
 		if (SysFileSystem.exists(path))
