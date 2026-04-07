0f8a5eebbfc292723681ec0cb751e755421e4f3d
Idklol55 <95130098+Idklol55@users.noreply.github.com>
2026-04-07 07:21:13 +0700

    Update unix.sh

--- setup/unix.sh
+++ setup/unix.sh
@@ -7,7 +7,7 @@ echo Makking the main haxelib and setuping folder in same time..
 mkdir ~/haxelib && haxelib setup ~/haxelib
 echo Installing dependencies...
 echo This might take a few moments depending on your internet speed.
-haxelib install lime 8.1.2 --quiet
+haxelib git lime https://github.com/kittycathy233/lime --quiet
 haxelib run lime setup
 haxelib install openfl 9.3.3 --quiet
 haxelib install flixel 5.5.0 --quiet
@@ -15,7 +15,7 @@ haxelib git hscript-iris https://github.com/crowplexus/hscript-iris --quiet
 haxelib git flixel-animate https://github.com/MaybeMaru/flixel-animate --quiet
 haxelib git moonchart https://github.com/MaybeMaru/moonchart --quiet
 haxelib install hxvlc 1.9.2 --quiet
-haxelib install hxcpp 4.3.2 --quiet
+haxelib git hxcpp https://github.com/kittycathy233/hxcpp --quiet
 haxelib install hxdiscord_rpc 1.2.4 --quiet
 haxelib install hxjson5 --quiet
 haxelib install flixel-addons 3.0.2 --quiet
